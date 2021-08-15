7369caedc1c0d284aefac05c2f10d092dfc1bed9
Rishika Singh <rishikasingh0711@gmail.com>
2021-08-15 20:22:11 +0530

    second commit

--- routes.js
+++ routes.js
@@ -1,15 +1,64 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/name',(req , res) => {
-    res.send({
-        name:'rihika',
-        class:'tets',
-    });
-});
+// router.get('/name',(req , res) => {
+//     res.send({
+//         name:'rihika',
+//         class:'tets',
+//     });
+// });
 
 router.post('/',(req , res) => {
     res.send(" put hello");
 });
 
-module.exports = router;
+//dynamic routes
+// router.get('/:id/users',(req, res) => {
+//     console.log(`this is the value - ${req.params.id}`);
+//     res.send({
+//        
+//         name:'rihika',
+//         class:'tets',
+//     });
+// });
+
+//using regular expression [Reg ex]=define a rule set for string or number
+
+// router.get('/:id([0-9])',(req, res) => {
+//     console.log(`this is the value - ${req.params.id}`);
+//     res.send({
+//        // name:'hello', rts = router;
+//         name:'rihika',
+//         class:'tets',
+//     });
+// });
+
+// router.get('/:id([0-9]{5})',(req, res) => {
+//     console.log(`this is the value - ${req.params.id}`);
+//     res.send({
+//        // name:'hello', rts = router;
+//         name:'rihika',
+//         class:'tets',
+//     });
+// });
+
+// router.get('/:id(*)',(req, res) => {
+//     console.log(`this is the value - ${req.params.id}`);
+//     res.send({
+//        // name:'hello', rts = router;
+//         name:'rihika',
+//         class:'tets',
+//     });
+// });
+
+
+// router.get('/:id([A-Z])',(req, res) => {
+//         console.log(`this is the value - ${req.params.id}`);
+//         res.send({
+//            // name:'hello', rts = router;
+//             name:'rihika',
+//             class:'tets',
+//         });
+//     });
+
+module.exports = router; 
